894ea067fec9b59a0ec8ee27838ccd0fdb3b3400
Steven Lee Taylor <47579176+Leepoppa@users.noreply.github.com>
2019-04-02 11:32:38 -0700

    changes to modal

--- midterm/main.js
+++ midterm/main.js
@@ -1,9 +1,6 @@
 
 $(document).ready(function(){
-    $('.carousel').carousel({
-        interval:   2000, 
-        keyboard:   true
-    });
+    $('.carousel').carousel({interval: 3000, keyboard:true});
 
     $('.carousel-pauser').click(function(){
         $('.carousel').carousel('pause');
@@ -12,7 +9,11 @@ $(document).ready(function(){
     $('.carousel').on('slid.bs.carousel', function(event){
         console.log(event);
     });
-
+    $('.modal-popper').click(function(){
+        $('.modal').modal({
+            backdrop:'static'
+        });
+    
 
 }); 
 
