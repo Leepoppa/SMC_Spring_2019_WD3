de48ea796ceeabc9f3fb76f05e6791875deb9cc4
Steven Lee Taylor <47579176+Leepoppa@users.noreply.github.com>
2019-05-28 06:26:40 -0700

    final project
    full website for tunic hut

---
+++ final/myscripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var target = document.location.hash.replace("#", "");
+    if (target.length) {
+        if(target=="option1"){
+          showModal("title1","content1");
+        }
+        else if(target=="option2"){
+            showModal("title2","content2");
+        }
+    }else{
+        showModal("no title","no content");
+    }
+
+    function showModal(title,content){
+        $('#myModal .modal-title').html(title);
+        $('#myModal .modal-body').html(content);
+        $('#myModal').modal('show');
+    }
+    
+});
